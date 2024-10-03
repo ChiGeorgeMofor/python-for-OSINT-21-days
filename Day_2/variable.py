7a67c46061fe6dd419f612fd8c8ac595bf59ff9a
Chi George Mofor <chigeorgemofor@gmail.com>
2024-10-03 09:54:27 +0100

    adding a nickname

--- Day_2/variable.py
+++ Day_2/variable.py
@@ -1,4 +1,8 @@
 first_name = "John"
 last_name = input('What is your last name?\n')
 print("You are" + " " + first_name+ " " + last_name)
- 
+name = first_name + ' ' + last_name
+#print(name)
+nickname = input('What is your nickaname? \n')
+
+print("Your names are" + " " + name + " AKA" + " " + nickname)
