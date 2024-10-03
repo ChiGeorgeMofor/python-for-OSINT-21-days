196e36be8d03dd3efd14709070b9f917c77bff48
Chi George Mofor <chigeorgemofor@gmail.com>
2024-10-03 10:18:40 +0100

    adding some conditions

--- Day_2/condition.py
+++ Day_2/condition.py
@@ -1,7 +1,15 @@
 age = input('How old are you?\n')
 
 
-if int(age) > 27:
+'''if int(age) > 27:
     print("You are so old")
 elif int(age) < 27:
-    print("You are so young")
+    print("You are so young")'''
+
+
+if int(age) > 50:
+    print("You are an old nigga")
+elif int(age) < 27:
+    print("You might still be young")
+else:
+    print("You are average")
