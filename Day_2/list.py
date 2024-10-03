74c7e4145c1a3c5671853f1c9870c4f49dd0b8e9
Chi George Mofor <chigeorgemofor@gmail.com>
2024-10-03 10:34:40 +0100

    checking on boys too

--- Day_2/list.py
+++ Day_2/list.py
@@ -5,3 +5,11 @@ girls.append("Brenda")
 print(girls)
 
 print (girls[3])
+
+girls.sort(reverse = True)
+print(girls)
+print('\n')
+boys = ["George", "Bello", "Joel", "Bankey"]
+print(boys)
+boys.sort(reverse=True, key=len)
+print(boys)
