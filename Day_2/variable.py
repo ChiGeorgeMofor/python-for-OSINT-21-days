b3aef027ea51e2977acc6cf69d685e9caf600a58
ChiGeorgeMofor <105949693+ChiGeorgeMofor@users.noreply.github.com>
2024-10-05 01:34:43 +0100

    Update variable.py

--- Day_2/variable.py
+++ Day_2/variable.py
@@ -3,6 +3,6 @@ last_name = input('What is your last name?\n')
 print("You are" + " " + first_name+ " " + last_name)
 name = first_name + ' ' + last_name
 #print(name)
-nickname = input('What is your nickaname? \n')
+nickname = input('What is your nickaname? \n') 
 
 print("Your names are" + " " + name + " AKA" + " " + nickname)
